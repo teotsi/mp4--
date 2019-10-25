652bffe42af1a8a9642a5b1364c1ad331e8ca980
teotsi <teotsi@gmail.com>
2019-10-25 18:55:19 +0300

    form comm works

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '.'
-app.secret_key = "secret key"
+app.secret_key = "qwerty"
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print("holaaa")
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -23,9 +22,24 @@ def index():
         if file:
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect('edit.html')
+            audiofile = eyed3.load("/home/p3160180/Downloads/test.mp3")
+            flash(audiofile.tag.title)
+            flash(audiofile.tag.artist)
+            flash(audiofile.tag.album)
+            return render_template('edit.html')
     else:
         return render_template('index.html')
+
+
+@app.route('/edit', methods=['POST'])
+def edit():
+    if request.method == 'POST':
+        new_title = request.form['title']
+        new_artist = request.form['artist']
+        new_album = request.form['album']
+        print("yeaaaaaaaaheeee")
+        return new_artist
+
 
 if __name__ == '__main__':
     app.run()
