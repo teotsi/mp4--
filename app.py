0d2add7f8d0be30138a4223dee609b35e9d6b47a
teotsi <teotsi@gmail.com>
2019-11-02 18:15:44 +0200

    added comments

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ from flask import Flask, render_template, request, redirect, flash, send_file
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/files'
 app.secret_key = "qwerty"
-extensions = ['mp4', 'avi', 'mp3']
+extensions = ['mp4', 'avi']
 
 
-def valid_file_extension(filename):
+def valid_file_extension(filename):  # checking if file is a video file
     if '.' in filename:
         if filename.split('.')[-1] in extensions:
             return True
     return False
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])  # index route
 def index():
-    if request.method == 'POST':
+    if request.method == 'POST':  # POST is activated on file upload
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
@@ -33,25 +33,25 @@ def index():
         if file:
             filename = file.filename
             if valid_file_extension(filename):
-                if not os.path.exists(app.config['UPLOAD_FOLDER']):
+                if not os.path.exists(app.config['UPLOAD_FOLDER']):  # creating file upload folder if it doesn't exist
                     os.makedirs(app.config['UPLOAD_FOLDER'])
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                mixed_song_name = filename.split('.')[0]
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))  # saving file
+                mixed_song_name = filename.split('.')[0]  # removing file extension
                 print(mixed_song_name)
-                tokens = re.split('[_-]', mixed_song_name)
+                tokens = re.split('[_-]', mixed_song_name)  # creating possible tokens
                 print(tokens)
-                for token in tokens:
+                for token in tokens:  # flashing tokens to use in html file
                     print("flashing")
                     flash(token)
-                audio_file_id = hashlib.md5(
+                audio_file_id = hashlib.md5(  # creating a unique file id for uploaded file based on file size
                     (mixed_song_name + str(request.cookies.get('filesize'))).encode()).hexdigest()
                 audio_file = app.config['UPLOAD_FOLDER'] + '/' + audio_file_id + '.mp3'
                 print(audio_file)
                 print(filename)
-                clip = mp.VideoFileClip(app.config['UPLOAD_FOLDER'] + '/' + filename)
-                clip.audio.write_audiofile(audio_file)
+                clip = mp.VideoFileClip(app.config['UPLOAD_FOLDER'] + '/' + filename)  # reading video file
+                clip.audio.write_audiofile(audio_file)  # extracting audio file
                 print("moving on")
-                return render_template('edit.html', song_id=audio_file_id)
+                return render_template('edit.html', song_id=audio_file_id)  # responding with edit screen
             else:
                 flash("invalid extension")
                 return redirect(request.url)
@@ -61,24 +61,22 @@ def index():
 
 @app.route('/edit/<string:id>', methods=['POST'])
 def edit(id=None):
-    if id == None:
-        return "hehehehehe"
-    if request.method == 'POST':
-        new_title = request.form['title']
+    if request.method == 'POST':  # POST is activated on save button click
+        new_title = request.form['title']  # getting applied song tags
         new_artist = request.form['artist']
         new_album = request.form['album']
-        audio_file = app.config['UPLOAD_FOLDER'] + '/' + id + '.mp3'
-        song = eyed3.load(audio_file).tag
-        song.title = new_title
+        audio_file = app.config['UPLOAD_FOLDER'] + '/' + id + '.mp3'  # getting file name
+        song = eyed3.load(audio_file).tag  # loading mp3 file
+        song.title = new_title  # setting new tags
         song.artist = new_artist
         song.album = new_album
-        song.save()
-        if new_title is not None and new_artist is not None:
+        song.save()  # saving mp3 file
+        if new_title is not None and new_artist is not None:  # if the user provided title and artist we rename the file
             new_name = app.config['UPLOAD_FOLDER'] + '/' + new_artist + '-' + new_title + '.mp3'
             shutil.copy(audio_file, new_name)
             audio_file = new_name
-        return send_file(audio_file, as_attachment=True)
+        return send_file(audio_file, as_attachment=True)  # sending the file to user
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # initializing app
     app.run()
